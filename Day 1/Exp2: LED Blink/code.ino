2aacf77e89aebbfaa6e111604faa65a2094aa577
The Electronics Club @ SASTRA <teamtecs.sastra@gmail.com>
2026-03-13 22:30:22 +0530

    Create code.ino

---
+++ Day 1/Exp2: LED Blink/code.ino
@@ -0,0 +1,12 @@
+//declare the pin for LED
+
+void setup() {
+   //write PIN mode
+}
+
+void loop() {
+    //write instruction for LED ON
+    delay(500);
+    //write instruction for LED OFF
+    delay(500);
+}
