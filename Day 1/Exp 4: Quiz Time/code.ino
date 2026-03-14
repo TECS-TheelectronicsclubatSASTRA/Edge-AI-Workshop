6788677e0965d31dd399d0acdf69ff8e92fcb7e4
The Electronics Club @ SASTRA <teamtecs.sastra@gmail.com>
2026-03-14 12:03:14 +0530

    Update code.ino

--- Day 1/Exp 4: Quiz Time/code.ino
+++ Day 1/Exp 4: Quiz Time/code.ino
@@ -1,1 +1,21 @@
-Try to tune your logic to that particular question frequency to get the answer !!
+int ledPin = 2;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  static float x=0;
+  float y = 100*sin(x);
+  Serial.println(y);
+  if(y>0){
+      digitalWrite(ledPin, HIGH);
+    }
+    else{
+        digitalWrite(ledPin, LOW);
+      }
+  x+=0.1; 
+  delay(20);
+}
