74324cb5df9c50f2e2b14c46e754e7f87a60412c
The Electronics Club @ SASTRA <teamtecs.sastra@gmail.com>
2026-03-15 10:37:21 +0530

    Update code.ino

--- Day 1/Exp 7:MPU interfacing /code.ino
+++ Day 1/Exp 7:MPU interfacing /code.ino
@@ -9,9 +9,9 @@ void setup() {
 
   Serial.println("Initializing MPU6050...");
 
-  // Initialize MPU6050
+  mpu.initialize();
 
-  if (// mpu6050 test connection) {
+  if (mpu.testConnection()) {
     Serial.println("MPU6050 connection successful!");
   } else {
     Serial.println("MPU6050 connection failed!");
@@ -23,7 +23,47 @@ void loop() {
   int16_t ax, ay, az;
   int16_t gx, gy, gz;
 
-  // get the motion 
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+ 
+  Serial.print("Accel: ");
+  Serial.print(ax); Serial.print(" ");
+
+  Serial.print(ay); Serial.print(" ");
+  Serial.print(az); Serial.print("  |  ");
+
+  Serial.print("Gyro: ");
+  Serial.print(gx); Serial.print(" ");
+  Serial.print(gy); Serial.print(" ");
+  Serial.println(gz);
+
+  delay(500);
+}
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(21, 22);  // SDA, SCL
+
+  Serial.println("Initializing MPU6050...");
+
+  mpu.initialize();
+
+  if (mpu.testConnection()) {
+    Serial.println("MPU6050 connection successful!");
+  } else {
+    Serial.println("MPU6050 connection failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  int16_t ax, ay, az;
+  int16_t gx, gy, gz;
+
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
  
   Serial.print("Accel: ");
   Serial.print(ax); Serial.print(" ");
